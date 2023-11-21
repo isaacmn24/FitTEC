b652efade2dfe813ee694763937fc96ce1c6eb2c
Ísaac <isaacfelipe2007@gmail.com>
2023-11-20 20:56:39 -0600

    Agregué el código funcional del sensor de pulsos y de temperatura.

---
+++ sensores/sensores.ino
@@ -0,0 +1,79 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+hw_timer_t * sampleTimer = NULL;
+portMUX_TYPE sampleTimerMux = portMUX_INITIALIZER_UNLOCKED;
+
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+
+//  Variables
+const int PulseWire = 36;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+                               // Otherwise leave the default "550" value. 
+                               
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
+void IRAM_ATTR onSampleTime() {
+  portENTER_CRITICAL_ISR(&sampleTimerMux);
+    PulseSensorPlayground::OurThis->onSampleTime();
+  portEXIT_CRITICAL_ISR(&sampleTimerMux);
+}
+
+//Sensor de Tempertura
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;     
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup() {   
+
+  Serial.begin(115200);          // For Serial Monitor
+ // Temperatura
+  // Start the DS18B20 sensor
+  sensors.begin();
+ //////
+
+  // Configure the PulseSensor object, by assigning our variables to it. 
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.setThreshold(Threshold);   
+
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
+   if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+  }
+
+  sampleTimer = timerBegin(0, 80, true);                
+  timerAttachInterrupt(sampleTimer, &onSampleTime, true);  
+  timerAlarmWrite(sampleTimer, 2000, true);      
+  timerAlarmEnable(sampleTimer);
+}
+
+
+void loop() {
+
+///////////////////////7 trmperatura
+   sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+////////////////////
+
+if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
+int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+                                               // "myBPM" hold this BPM value now. 
+ Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
+ Serial.print("BPM: ");                        // Print phrase "BPM: " 
+ Serial.println(myBPM);                        // Print the value inside of myBPM. 
+}
+                   // considered best practice in a simple sketch.
+
+delay(1000);
+}
+
+  
