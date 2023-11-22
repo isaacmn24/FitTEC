71a215df5e863af93a2671cc97387db0ed8003c6
Ísaac <isaacfelipe2007@gmail.com>
2023-11-21 22:56:00 -0600

    Corregido el pin final del buzzer

--- Bluetooth/Bluetooth.ino
+++ Bluetooth/Bluetooth.ino
@@ -26,7 +26,7 @@ int maxBPM = 180;
 int maxTemp = 38;
 
 // ALARMA
-#define pinBuzzer 12
+#define pinBuzzer 14
 
 // BLUETOOTH
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
